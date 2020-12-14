17b3a77c13ca285e578333d428f1c1bca068806b
Laurent Broudoux <laurent.broudoux@gmail.com>
2020-12-14 17:14:24 +0100

    Fixing default parameters

--- async-clients/kafkajs-client/avro-consumer.js
+++ async-clients/kafkajs-client/avro-consumer.js
@@ -7,7 +7,7 @@ const { Kafka } = require('kafkajs')
 var commandArgs = process.argv.slice(2);
 
 const kafkaHost = commandArgs[0] || 'localhost:9092';
-const kafkaTopic = commandArgs[1] || 'UsersignedupAPI_0.1.1_user-signedup';
+const kafkaTopic = commandArgs[1] || 'UsersignedupAvroAPI_0.1.2_user-signedup';
 const KafkaCert = commandArgs[2] || null;
 
 console.log("Connecting to " + kafkaHost + ' on topic ' + kafkaTopic);
